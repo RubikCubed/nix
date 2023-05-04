c64af86a23912f12787644213422e93ce2556b67
Aidan <spider@tilde.cafe>
2023-05-04 18:30:07 +1000

    add haskell

--- home.nix
+++ home.nix
@@ -1,5 +1,45 @@
 { config, pkgs, helix, ... }:
 
+let haskell = pkgs.haskell.packages.ghc92;
+    packages = p: with p; [
+/*  adjunctions
+    aeson
+    async
+    base
+    bytestring
+    comonad
+    constraints
+    containers
+    contravariant
+    criterion
+    data-fix
+    distributive
+    effectful
+    exceptions
+    free
+    foldl
+    kan-extensions
+    lens
+    megaparsec
+    mtl
+    parser-combinators
+*/  pretty-simple
+/*  prettyprinter
+    primitive
+    profunctors
+    QuickCheck
+    random
+    recursion-schemes
+    semigroupoids
+    text-short
+    stm
+    template-haskell
+    text
+    text-show
+    transformers
+    unordered-containers
+*/  ];
+in
 {
   home.username = "mate";
   home.homeDirectory = "/home/mate";
@@ -10,7 +50,16 @@
     ripgrep
     fd
     wget
-  ];
+  ] ++ (with haskell; [
+    cabal-fmt
+    cabal-install
+    cabal-plan
+    fourmolu
+    (ghcWithHoogle packages)
+    haskell-language-server
+    hlint
+    ghcid
+  ]);
 
   programs = {
     bat.enable = true;
@@ -116,6 +165,28 @@
     '';
   };
 
+
+  home.file = {
+    ".ghc/ghci.conf".text = ''
+      :def hoogle \x -> pure $ ":!hoogle \"" <> x <> "\" --count=20"
+      :def! doc \x -> pure $ ":!hoogle --info \"" ++ x ++ "\""
+      :set prompt "> "
+      :set prompt-cont "  | "
+      :set -XNoStarIsType
+      :set -Wno-unused-top-binds
+      :set +m
+
+      :seti -Wall -Wno-type-defaults -Wno-name-shadowing
+      :seti -fno-defer-type-errors -fno-show-valid-hole-fits
+      :set -package pretty-simple
+      :seti -interactive-print=Text.Pretty.Simple.pPrint
+    '';
+    ".haskeline".text = "editMode: Vi";
+  };
+
+  home.sessionVariables.CABAL_DIR = "${config.xdg.dataHome}/cabal";
+  home.sessionPath = ["${config.xdg.dataHome}/cabal/bin"];
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
@@ -125,5 +196,4 @@
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "22.11";
-
 }
